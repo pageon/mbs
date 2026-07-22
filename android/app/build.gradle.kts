d67ca7edc542c017dfb270e5a29e9ce53dbdce45
Jelmer Prins <jelmer@pageon.be>
2026-07-22 18:23:28 +0200

    Bump Android version to 1.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "be.pageon.mentalbucketsystem"
         minSdk = 21
         targetSdk = 35
-        versionCode = 10100
-        versionName = "1.1.0"
+        versionCode = 10101
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
